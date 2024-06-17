25204ba0c4c43c7e82730bd7ca3b6dc12e79dd20
Joao Teles <joaojoseteles@gmail.com>
2024-06-17 07:44:58 -0300

    :tada: Initial commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import {fastify} from "fastify";
+
+const app = fastify()
+
+app.get("/", async() => {
+  return "Hello World!"
+});
+
+
+app.listen({
+  port:3333
+}).then(() => {
+  console.log('🚀 HTTP Server Running!')
+})
