afd153950a39de282e8d01b58cbe409fbba062a1
Joao Teles <joaojoseteles@gmail.com>
2024-06-17 08:05:57 -0300

    :ok_hand: style: lint in server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
-import {fastify} from "fastify";
+import { fastify } from 'fastify'
 
 const app = fastify()
 
-app.get("/", async() => {
-  return "Hello World!"
-});
-
-
-app.listen({
-  port:3333
-}).then(() => {
-  console.log('🚀 HTTP Server Running!')
+app.get('/', async () => {
+  return 'Hello World!'
 })
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP Server Running!')
+  })
